c9fa060162a9e33ee989b0e542c7d90909ec7ce4
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2022-10-05 19:08:00 +0200

    0-1 Knapsack using java

--- README.md
+++ README.md
@@ -141,5 +141,6 @@
 | 3   | [Ways to Decode](https://www.interviewbit.com/problems/ways-to-decode/)                                         | Amazon Facebook Google               | String DP | [java](https://github.com/EbrahimMohamed2611/Interviewbit/blob/main/src/main/java/com/interviewbit/amazone/dynamicProgramming/WaysToDecode.java)                 |
 | 4   | [Best Time to Buy and Sell Stocks I](https://www.interviewbit.com/problems/best-time-to-buy-and-sell-stocks-i/) | Amazon Facebook Google               | Stock DP  | [java](https://github.com/EbrahimMohamed2611/Interviewbit/blob/main/src/main/java/com/interviewbit/amazone/dynamicProgramming/WaysToDecode.java)                 |
 | 5   | [Chain of Pairs](https://www.interviewbit.com/problems/chain-of-pairs/)                                         | Amazon Directi                       | LIS DP    | [java](https://github.com/EbrahimMohamed2611/Interviewbit/blob/main/src/main/java/com/interviewbit/amazone/dynamicProgramming/WaysToDecode.java)                 |
+| 6   | [0-1 Knapsack](https://www.interviewbit.com/problems/0-1-knapsack/)                                             | Amazon Deshaw                        | SubSet DP | [java](https://github.com/EbrahimMohamed2611/Interviewbit/blob/main/src/main/java/com/interviewbit/amazone/dynamicProgramming/WaysToDecode.java)                 |
 
 </details>
---
+++ src/main/java/com/interviewbit/amazone/dynamicProgramming/_0_1Knapsack.java
@@ -0,0 +1,38 @@
+package com.interviewbit.amazone.dynamicProgramming;
+
+import java.util.*;
+
+public class _0_1Knapsack {
+
+ // Time Complixity : O(N * C)
+    // Space Complexity: O(N * C)
+    public int solve(ArrayList<Integer> values, ArrayList<Integer> weights, int capacity) {
+        int[][]dp = new int[values.size()+1][capacity+1];
+        return knapsack(0, values, weights, capacity,dp);
+    }
+    private int knapsack(int currentIndex, ArrayList<Integer> values, ArrayList<Integer> weights, int capacity,int[][]dp){
+
+        if(currentIndex == values.size()-1) // will reach the last item we can take if fit in the remian capacity
+        {
+            if(weights.get(currentIndex) <= capacity)
+                return values.get(currentIndex);
+            else
+                return 0;
+        }
+
+        if(dp[currentIndex][capacity] != 0)
+            return dp[currentIndex][capacity];
+        // first ray to take the current item
+        int profit1 = 0;
+        if(weights.get(currentIndex) <= capacity)
+            profit1 = values.get(currentIndex)+ knapsack(currentIndex+1, values, weights, capacity - weights.get(currentIndex),dp);
+
+        // second try not take the current Iitem
+        // the capacity will remain tha same and will move to next item
+        int profit2 = knapsack(currentIndex+1, values, weights, capacity, dp);
+
+        return dp[currentIndex][capacity] = Math.max(profit1, profit2);
+    }
+
+}
+
